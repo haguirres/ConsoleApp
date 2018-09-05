a6aa5f08ae3190cd6f1b922d39e75d6fba5c8510
Juan Carlos Espinosa Ortiz <juan.espinosa@mobiik.com>
2018-09-05 15:37:52 -0500

    Corrección al problema Cannot GET

--- School.Web/ClientApp/src/app/app.module.ts
+++ School.Web/ClientApp/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { GradeComponent } from './grade/grade.component';
     SchoolTypeComponent,
     StudentComponent,
     PersonComponent,
-    GradeComponent
+    GradeComponent,
     SchoolComponent,
     AddressComponent 
   ],
@@ -39,8 +39,8 @@ import { GradeComponent } from './grade/grade.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'school', component: SchoolComponent }
-      { path: 'address', component: AddressComponent }
+      { path: 'school', component: SchoolComponent },
+      { path: 'address', component: AddressComponent },
       { path: 'school', component: SchoolComponent },
       { path: 'school-type', component: SchoolTypeComponent }
 
