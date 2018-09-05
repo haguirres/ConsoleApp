44e0dfeccd6f02d04a6010eddf435c5566663be9
Juan Carlos Espinosa Ortiz <juan.espinosa@mobiik.com>
2018-09-05 18:11:31 -0500

    Schedule TERMINADO

--- School.Web/ClientApp/src/app/schedule/schedule.component.ts
+++ School.Web/ClientApp/src/app/schedule/schedule.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ischedule } from './model/ischedule';
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+  schedule = <Ischedule>{};
   constructor() { }
-
+  
   ngOnInit() {
+    
   }
 
 }
