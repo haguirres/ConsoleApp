07b4a43910821b605d1b9dae2448ecac7f88f507
Ulises <ulis2_rom@hotmail.com>
2018-09-05 09:24:03 -0500

    Cree Person y Cambie Nombre de person a SchoolUSer

--- SchoolDatabase/dbo/Tables/Person.sql
+++ SchoolDatabase/dbo/Tables/Person.sql
@@ -1,9 +1,4 @@
-﻿CREATE TABLE [dbo].[Person] (
-    [PersonId] INT          NOT NULL,
-    [RolId]    INT          NOT NULL,
-    [UserName] VARCHAR (50) NOT NULL,
-    [Password] VARCHAR (15) NOT NULL,
-    PRIMARY KEY CLUSTERED ([PersonId] ASC),
-    CONSTRAINT [FK_PERSON_ROLES] FOREIGN KEY ([RolId]) REFERENCES [dbo].[Roles] ([RolId])
-);
-
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ SchoolDatabase/dbo/Tables/SchoolUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [PersonId] INT          NOT NULL,
+    [RolId]    INT          NOT NULL,
+    [UserName] VARCHAR (50) NOT NULL,
+    [Password] VARCHAR (15) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PersonId] ASC),
+    CONSTRAINT [FK_PERSON_ROLES] FOREIGN KEY ([RolId]) REFERENCES [dbo].[Roles] ([RolId])
+);
+
