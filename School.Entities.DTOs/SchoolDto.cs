e3eadd0d8822c5050621114b4e0be276d8dfc8f6
ViriMares <virimaresoui@gmail.com>
2018-09-07 17:28:11 -0500

    Servicio para recuperar una lista de escuelas

---
+++ School.Entities.DTOs/SchoolDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace School.Entities.DTOs
+{
+    public class SchoolDto
+    {
+        public string SchoolId { get; set; }
+        public int AddressId { get; set; }
+        public string SchoolName { get; set; }
+        public int SchoolType { get; set; }
+        public int MinToPass { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
