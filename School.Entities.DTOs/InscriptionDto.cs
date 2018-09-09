fa8421e91d6b9cfaec382119379255a8aadfc434
Sebastian Pioquinto Cruz <sebastian.pioquinto@mobiik.com>
2018-09-08 23:40:52 -0500

    Agregados Capa de Services y capa de Bussines para Inscription. Modificado DTO Y dataAccess para mejor funcionamiento de Inscription.

--- School.Entities.DTOs/InscriptionDto.cs
+++ School.Entities.DTOs/InscriptionDto.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace School.Entities.DTOs
 {
-    class InscriptionDto
+    public class InscriptionDto
     {
         public int StudentId { get; set; }
         public string SchoolId { get; set; }
