2ac4845098c83925857273fddf0bd35f933d98b8
Juan Carlos Espinosa Ortiz <juan.espinosa@mobiik.com>
2018-09-10 16:38:46 -0500

    SchoolUseId (renombrado) en tabla SchoolUser. (Actualizar base de datos local)

--- SchoolDatabase/dbo/Tables/SchoolUser.sql
+++ SchoolDatabase/dbo/Tables/SchoolUser.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[SchoolUser] (
-    [PersonId] INT          NOT NULL IDENTITY,
+    [SchoolUserId] INT          NOT NULL IDENTITY,
     [RolId]    INT          NOT NULL,
     [UserName] VARCHAR (50) NOT NULL,
     [Password] VARCHAR (15) NOT NULL,
     [IsActive] BIT NOT NULL DEFAULT 1, 
-    PRIMARY KEY CLUSTERED ([PersonId] ASC),
+    PRIMARY KEY CLUSTERED ([SchoolUserId] ASC),
     CONSTRAINT [FK_PERSON_ROLES] FOREIGN KEY ([RolId]) REFERENCES [dbo].[Roles] ([RolId])
 );
 
