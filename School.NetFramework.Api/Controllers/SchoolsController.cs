901fb90729dff607d460f70af58a964821e81aa7
ViriMares <virimaresoui@gmail.com>
2018-09-10 17:24:33 -0500

    Cambio a servicio web api school y creación de schools service de frontend.

--- School.NetFramework.Api/Controllers/SchoolsController.cs
+++ School.NetFramework.Api/Controllers/SchoolsController.cs
@@ -6,11 +6,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 
 namespace School.NetFramework.Api.Controllers
 {
-    [Route("api/schools")]
+    [EnableCors("*", "*", "*")]
     public class SchoolsController : ApiController
     {
         ProcessSchools processSchools;
