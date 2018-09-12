35bfb1b12fbcc3950de4c2e46e369d570851f953
Sebastian Pioquinto Cruz <sebastian.pioquinto@mobiik.com>
2018-09-12 14:48:14 -0500

    Actualizacion de el campo IsActive de la tabla SchoolUser. Se agrego valor por defautl = 1

--- SchoolDatabase/dbo/Tables/SchoolUser.sql
+++ SchoolDatabase/dbo/Tables/SchoolUser.sql
@@ -3,10 +3,12 @@
     [RolId]        INT          NOT NULL,
     [UserName]     VARCHAR (50) NOT NULL,
     [Password]     VARCHAR (15) NOT NULL,
-    [IsActive]     BIT          NOT NULL,
+    [IsActive]     BIT          DEFAULT ('true') NOT NULL,
     CONSTRAINT [PK_SchoolUser] PRIMARY KEY CLUSTERED ([SchoolUserId] ASC),
     CONSTRAINT [FK_PERSON_ROLES] FOREIGN KEY ([RolId]) REFERENCES [dbo].[Roles] ([RolId])
 );
+
+
 
 
 
